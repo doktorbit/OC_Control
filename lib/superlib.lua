885887a06ecdb3de1eb7c9e1d5dc95bc68b7d014
doktorbit <tim@effeff-infomedia.de>
2015-07-14 09:08:26 +0200

    Added Superlib.lua Added versions.lua
    Signed-off-by: doktorbit <tim@effeff-infomedia.de>

---
+++ lib/superlib.lua
@@ -0,0 +1,60 @@
+local version = "0.0.1"
+local m = {}
+
+local internet = require("internet")
+local term = require("term")
+local keyboard = require("keyboard")
+local event = require("event")
+local io = require("io")
+local string = require("string")
+local text = require("text")
+local wget = loadfile("/bin/wget")
+
+if component.isAvailable("internet") then
+	internet = true
+end
+
+local function downloadRaw(url)
+	assert(internet)
+	local sContent = ""
+	local result, response = pcall(internet.request, url)
+	if not result then
+		return nil
+	end
+	for chunk in response do
+		sContent = sContent..chunk
+	end
+	return sContent
+end
+
+local function downloadFile(url, path)
+	assert(internet)
+	return wget("-fg", url, path)
+end
+
+function m.getVersion()
+	return version
+end
+
+function m.checkVersion()
+	response = downloadFile("https://raw.githubusercontent.com/OC_Control/doktorbit/master/versions.lua", "/tmp/versions.lua")
+	versions = loadFile("/tmp/versions.lua")
+	return versions, version
+end
+
+function m.downloadFile(url, path)
+	local success, response = pcall(downloadFile, url, path)
+	if not success then
+		return nil
+	end
+	return response
+end
+
+function m.download(url)
+	local success, response = pcall(downloadRaw, url)
+	if not success then
+		return nil
+	end
+	return response
+end
+	
